78b87b30b7e0453ba3efa44a31623f7a88494dad
Joshua Markle <joshuamarkle25@gmail.com>
2025-10-09 10:20:56 -0400

    Use Postgres heroku db in prodution

--- tradingapp/settings.py
+++ tradingapp/settings.py
@@ -5,7 +5,7 @@ Django settings for tradingapp project.
 import os
 from pathlib import Path
 
-# Load secrets
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -68,12 +68,17 @@ WSGI_APPLICATION = "tradingapp.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if os.getenv("DATABASE_URL"):
+    # Running on Heroku or another environment with DATABASE_URL set
+    DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
+else:
+    # Local development: use SQLite
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
