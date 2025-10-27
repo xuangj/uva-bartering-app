eeee69bace396fc1cd12d28c720d2001e2da2b10
Escarlet <46622960+escarlether@users.noreply.github.com>
2025-10-27 16:46:40 -0400

    added a navbar and the template for the shop part

--- tradingapp/urls.py
+++ tradingapp/urls.py
@@ -21,6 +21,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("profile/", TemplateView.as_view(template_name="user.html"), name="profile"),
     path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
+ 
 ]
