42472887ee2e842c7f5be5d895c3dbc513520201
xuangj <xuangjin@gmail.com>
2025-11-13 20:15:33 -0500

    Accept Trade functionality works as expected and sends trade to Accepted tab on my_trades

--- core/views.py
+++ core/views.py
@@ -28,6 +28,7 @@ def home(request):
 @login_required
 def login_redirect_view(request):
     user = request.user
+
     if user.is_staff:
         return redirect("moderator_dashboard")
     else:
@@ -38,8 +39,10 @@ def login_redirect_view(request):
 @login_required
 def moderator_dashboard(request):
     user = request.user
+    profiles = Profile.objects.all()
+
     if user.is_staff:
-        return render(request, "moderator_dashboard.html")
+        return render(request, "moderator_dashboard.html", {"profiles": profiles})
     return HttpResponseForbidden("You are not allowed to access this page.")
 
 
@@ -83,7 +86,7 @@ def post_create(request):
         form = PostForm()
 
     # Render the template
-    return render(request, "post_form.html", {"form": form})
+    return render(request, "post_form.html", {"form": form, "is_edit": False})
 
 
 @login_required
@@ -113,6 +116,50 @@ def view_post(request, post_id: int) -> HttpResponse:
     return render(request, "post.html", {'post': post, 'form': form, 'existing_trade': existing_trade})
 
 
+# Edits existing post
+@login_required
+def edit_post(request, post_id: int):
+    post = get_object_or_404(Post, id=post_id)
+
+    if Trade.objects.filter(item_requested=post, status='Accepted').exists():
+        return HttpResponseForbidden("You can't edit a post after it's been traded.")
+    
+    if post.poster != request.user:
+        return HttpResponseForbidden("You can only edit your own posts.")
+    
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            edited_post = form.save(commit=False)
+            edited_post.poster = post.poster
+            edited_post.save()
+            messages.success(request, "Post updated successfully!")
+            return redirect("view_post", post_id=post.id)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, "post_form.html", {"form": form, "is_edit": True})
+
+
+# Delete post
+@login_required
+def delete_post(request, post_id: int):
+    post = get_object_or_404(Post, id=post_id)
+
+    if Trade.objects.filter(item_requested=post, status='Accepted').exists():
+        return HttpResponseForbidden("You can't delete a post after it's been traded.")
+    
+    if request.user.is_superuser:
+        post.delete()
+        return redirect("home")
+
+    if post.poster == request.user:
+        post.delete()
+        return redirect("home")    
+
+    return HttpResponseForbidden("You can only delete your own posts")
+        
+
 # --- Profile --- #
 
 @login_required
@@ -137,8 +184,17 @@ def edit_profile(request, username):
     )
 
 
+@login_required
+def delete_profile(request, profile_id):
+    profile = get_object_or_404(Profile, id=profile_id)
+
+
+
+
+    
 # --- Trades --- #
 
+# See all active trades (offers and listings)
 @login_required
 def my_trades(request) -> HttpResponse:
     user = request.user
@@ -160,6 +216,8 @@ def my_trades(request) -> HttpResponse:
 
     return render(request, "my_trades.html", {"pending_trades": pending_trades, "accepted_trades": accepted_trades, "denied_trades": denied_trades})
 
+
+# view individual trade, see original post and possible offer
 @login_required
 def view_trade(request, trade_id: int) -> HttpResponse:
     trade = get_object_or_404(Trade, id=trade_id)
@@ -169,6 +227,7 @@ def view_trade(request, trade_id: int) -> HttpResponse:
     return render(request, "trade.html", {'trade': trade, 'post': post})
 
 
+# OP can accept trade offer
 @login_required
 def accept_trade(request, trade_id):
     trade = get_object_or_404(Trade, id=trade_id)
@@ -178,13 +237,14 @@ def accept_trade(request, trade_id):
     
     trade.status = 'Accepted'
     trade.item_requested.is_available = False
-    trade.item_requested.save() 
+    trade.save() 
 
     messages.success(request, "Trade Accepted!", extra_tags="trade")
 
     return redirect('my_trades')
 
 
+# OP can decline trade offer
 @login_required
 def deny_trade(request, trade_id):
     trade = get_object_or_404(Trade, id=trade_id, userTwo=request.user)
@@ -193,6 +253,7 @@ def deny_trade(request, trade_id):
     return redirect('my_trades')
 
 
+# Offerer can rescind their offer
 @login_required
 def delete_trade_offer(request, trade_id):
     trade = get_object_or_404(Trade,id=trade_id, userOne=request.user)
